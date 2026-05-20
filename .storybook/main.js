408bfda3d03b8e679374563fc45530ede13db5e3
Russell Warner <rewarner@russellbits.com>
2026-05-20 14:30:27 -0400

    📦 Storybook for html installed. 🙈 Gitignore set to ignore node_modules.

---
+++ .storybook/main.js
@@ -0,0 +1,11 @@
+
+
+/** @type { import('@storybook/html-vite').StorybookConfig } */
+const config = {
+  "stories": [
+    "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"
+  ],
+  "addons": [],
+  "framework": "@storybook/html-vite"
+};
+export default config;
